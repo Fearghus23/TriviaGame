9ea06babca14edc4e4c9e78876ce72fed1a779b5
Fearghus Tyler <Fearghus333@gmail.com>
2019-08-24 19:18:05 -0700

    me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,9 @@
 $(document).ready(function(){
-  
   // event listeners
   $("#remaining-time").hide();
   $("#start").on('click', trivia.startGame);
   $(document).on('click' , '.option', trivia.guessChecker);
-  
 })
-
 var trivia = {
   correct: 0,
   incorrect: 0,
@@ -52,10 +49,8 @@ var trivia = {
     $('#results').html('');
     $('#timer').text(trivia.timer);
     $('#start').hide();
-
     $('#remaining-time').show();
     trivia.nextQuestion();
-    
   },
   nextQuestion : function(){
     trivia.timer = 10;
@@ -66,12 +61,10 @@ var trivia = {
     }
     var questionContent = Object.values(trivia.questions)[trivia.currentSet];
     $('#question').text(questionContent);
-
     var questionOptions = Object.values(trivia.options)[trivia.currentSet];
     $.each(questionOptions, function(index, key){
       $('#options').append($('<button class="option btn btn-info btn-lg">'+key+'</button>'));
     })
-    
   },
   timerRunning : function(){
     if(trivia.timer > -1 && trivia.currentSet < Object.keys(trivia.questions).length){
@@ -95,46 +88,33 @@ var trivia = {
         '<p>Incorrect: '+ trivia.incorrect +'</p>'+
         '<p>Unaswered: '+ trivia.unanswered +'</p>'+
         '<p>Please play again!</p>');
-
       $('#game').hide();
       $('#start').show();
     }
-    
   },
-
   guessChecker : function() {
-    
     var resultId;
-    
     var currentAnswer = Object.values(trivia.answers)[trivia.currentSet];
-    
     if($(this).text() === currentAnswer){
-      $(this).addClass('btn-success').removeClass('btn-info');
-      
+      $(this).addClass('btn-success').removeClass('btn-info'); 
       trivia.correct++;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 1000);
       $('#results').html('<h3>Correct Answer!</h3>');
     }
     else{
-      $(this).addClass('btn-danger').removeClass('btn-info');
-      
+      $(this).addClass('btn-danger').removeClass('btn-info');  
       trivia.incorrect++;
       clearInterval(trivia.timerId);
       resultId = setTimeout(trivia.guessResult, 1000);
       $('#results').html('<h3>Better luck next time! '+ currentAnswer +'</h3>');
     }
-    
   },
   guessResult : function(){
-    
     trivia.currentSet++;
-    
     $('.option').remove();
     $('#results h3').remove();
-    
-    trivia.nextQuestion();
-     
+    trivia.nextQuestion();  
   }
 
 }
